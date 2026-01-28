eb1a77a4f6aeee25fb76c0479eeca1b8ece70eef
Koen <koen@nlnetlabs.nl>
2026-01-28 15:43:49 +0100

    Undo setting the edition to 2024

--- src/sync.rs
+++ src/sync.rs
@@ -166,8 +166,10 @@ pub fn read(path: &PathBuf) -> Result<Bytes, io::Error> {
 }
 
 fn create_file_with_path(path: &Path) -> Result<File, io::Error> {
-    if !path.exists() && let Some(parent) = path.parent() {
-        fs::create_dir_all(parent)?;
+    if !path.exists() {
+        if let Some(parent) = path.parent() {
+            fs::create_dir_all(parent)?;
+        }
     }
     File::create(path)
 }
